a9f1403cfc610e7abecac1f57a5ddde4b0b08c95
panashegwapedza <panashegwapedza@gmail.com>
2026-08-27 08:37:29 +0200

    test(flutter): verify basket capability error contract

---
+++ apps/customer_flutter/test/core/capabilities/basket_capability_error_contract_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:essentials_mart_customer/core/capabilities/basket_capability.dart';
+import 'package:essentials_mart_customer/core/errors/api_exception.dart';
+import 'package:essentials_mart_customer/features/commerce/data/commerce_models.dart';
+import 'package:essentials_mart_customer/features/commerce/data/commerce_repository.dart';
+
+void main() {
+  test('exposes the clean ApiException message', () async {
+    const message = 'Unable to load basket';
+    final capability = BasketCapability(_ThrowingBasketRepository(
+      const ApiException(
+        code: 'BASKET_LOAD_FAILED',
+        message: message,
+        statusCode: 503,
+      ),
+    ));
+
+    await expectLater(capability.load(), throwsA(isA<ApiException>()));
+
+    expect(capability.errorMessage, message);
+  });
+}
+
+class _ThrowingBasketRepository implements BasketRepository {
+  const _ThrowingBasketRepository(this.error);
+
+  final ApiException error;
+
+  @override
+  Future<Basket> getBasket() async => throw error;
+
+  @override
+  Future<Basket> addToBasket(String productId, int quantity) async =>
+      throw error;
+
+  @override
+  Future<Basket> removeFromBasket(String productId) async => throw error;
+}
