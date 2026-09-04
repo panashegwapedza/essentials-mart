eaf05ad3a720a0df7c39104b400ee0db33a065f8
panashegwapedza <panashegwapedza@gmail.com>
2026-09-04 21:19:29 +0200

    feat(auth): mount functional local customer authentication

--- apps/customer_web/src/main.tsx
+++ apps/customer_web/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AuthOverlay from './AuthOverlay';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <AuthOverlay />
   </StrictMode>,
 );
