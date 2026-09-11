b8feeeff7559b99f46f5192cffa5b1902cd48e94
panashegwapedza <panashegwapedza@gmail.com>
2026-09-11 08:51:45 +0200

    feat(customer-web): mount family-first catalogue chooser

--- apps/customer_web/src/main.tsx
+++ apps/customer_web/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import AuthOverlay from './AuthOverlay';
 import NotificationCenter from './NotificationCenter';
+import CatalogueVariants from './CatalogueVariants';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <CatalogueVariants />
     <AuthOverlay />
     <NotificationCenter />
   </StrictMode>,
